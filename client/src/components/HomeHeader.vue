cbe9e5163e5178225192122251b037bc5db7fef8
shch <shch.pc@gmail.com>
2023-12-03 18:18:56 +0900

    add back button

--- client/src/components/HomeHeader.vue
+++ client/src/components/HomeHeader.vue
@@ -1,22 +1,30 @@
 <script setup lang="ts">
 import { ref, watch } from 'vue'
+import { useRouter } from 'vue-router'
 
 const props = defineProps<{
   modelValue: string
 }>()
 const emit = defineEmits(['update:modelValue'])
-
+const router = useRouter()
 const searchWord = ref(props.modelValue)
-
 watch(searchWord, () => {
   emit('update:modelValue', searchWord.value)
 })
+
+const back = () => {
+  router.back()
+}
+const canBack = () => {
+  return !!window.history.state.back
+}
 </script>
 
 <template>
   <header>
     <div class="wrapper">
-      <input type="text" v-model="searchWord" />
+      <a v-if="canBack()" class="left" href="#" @click.prevent="back">&lt;</a>
+      <input class="center" type="text" v-model="searchWord" />
     </div>
   </header>
 </template>
@@ -29,11 +37,22 @@ header {
 .wrapper {
   height: 100%;
   width: 100%;
-  display: flex;
-  flex-direction: column;
+  display: grid;
+  grid-template-columns: 1fr 6fr 1fr;
   align-items: center;
   justify-content: center;
 }
+.wrapper > * {
+  grid-row: 1;
+}
+.left {
+  grid-column: 1;
+  text-align: center;
+}
+.center {
+  grid-column: 2;
+}
+
 input {
   font-size: x-large;
 }
