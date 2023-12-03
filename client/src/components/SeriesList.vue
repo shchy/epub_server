b48834428e0d3066e4033a72ba864991e436c15b
shch <shch.pc@gmail.com>
2023-12-03 17:35:51 +0900

    最近読んだ順（仮）

--- client/src/components/SeriesList.vue
+++ client/src/components/SeriesList.vue
@@ -5,18 +5,43 @@ import BookFace from '../components/BookFace.vue'
 import IntersectionList from '@/components/IntersectionList.vue'
 import { routeNames } from '@/router'
 import { useRouter } from 'vue-router'
+import { useStore } from '@/services/inject'
+import type { Bookmark } from '@/services/store'
 
 const props = defineProps<{
   searchWord: string
   series: SeriesInfo[]
 }>()
 
+const store = useStore()
+
 const filteredList = computed(() => {
-  if (props.searchWord == '') return props.series
-  const xs = props.series.filter((x) =>
-    x.title.toLocaleLowerCase().includes(props.searchWord.toLocaleLowerCase())
-  )
-  return xs
+  let xs = props.series
+  if (props.searchWord) {
+    xs = props.series.filter((x) =>
+      x.title.toLocaleLowerCase().includes(props.searchWord.toLocaleLowerCase())
+    )
+  }
+  const sorted = xs.sort((a, b) => {
+    const getLastTime = (series: SeriesInfo) => {
+      const lastTime = series.books
+        .map((book) => store.loadBookmark(book.id))
+        .reduce((unix: number, b: Bookmark | undefined) => {
+          if (!b) return unix
+          const time = b.timestamp
+          return Math.max(unix, time)
+        }, -1)
+      return lastTime
+    }
+    const alast = getLastTime(a)
+    const blast = getLastTime(b)
+    const descTime = blast - alast
+    if (descTime != 0) {
+      return descTime
+    }
+    return a.id > b.id ? 1 : -1
+  })
+  return sorted
 })
 
 const router = useRouter()
--- client/src/services/store.ts
+++ client/src/services/store.ts
@@ -1,17 +1,23 @@
 export interface Bookmark {
   id: string
   index: number
+  timestamp: number
 }
 
 const storeService = () => {
   const saveBookmark = (v: Bookmark) => {
+    v.timestamp = Math.floor(new Date().getTime() / 1000)
     localStorage.setItem(v.id, JSON.stringify(v))
   }
 
   const loadBookmark = (id: string) => {
     const v = localStorage.getItem(id)
     if (!v) return
-    return JSON.parse(v) as Bookmark
+    const bookmark = JSON.parse(v) as Bookmark
+    if (!bookmark.timestamp) {
+      bookmark.timestamp = 0
+    }
+    return bookmark
   }
 
   return {
