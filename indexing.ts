341fa307c9593c2713692dc7f16b2c86b7fd9881
yokota.shuichi <yokota.shuichi@cy-com.co.jp>
2024-11-03 13:04:47 +0900

    indexing skip exists

--- indexing.ts
+++ indexing.ts
@@ -4,6 +4,7 @@ import sharp from 'sharp';
 import { Book, CreateEpub, CreateEpubController } from './src/_services';
 
 const bookDir = './public/books';
+const indexFilePath = path.join(bookDir, 'index.json');
 
 const epubList = readdirSync(bookDir)
   .map((fspath) => {
@@ -17,7 +18,18 @@ const epubList = readdirSync(bookDir)
   .filter((filepath) => filepath.endsWith('.epub'));
 
 const books: Book[] = [];
+const beforeIndex = JSON.parse(
+  readFileSync(indexFilePath).toString('utf8')
+) as Book[];
+
 for (const epubFilepath of epubList) {
+  const epubFileName = path.basename(epubFilepath);
+  const findOne = beforeIndex.find((x) => x.filePath === epubFileName);
+  if (findOne) {
+    books.push(findOne);
+    continue;
+  }
+
   console.log('epubFilepath', epubFilepath);
   const fileData = readFileSync(epubFilepath);
   const epub = CreateEpub(fileData);
@@ -30,13 +42,10 @@ for (const epubFilepath of epubList) {
   books.push({
     id: epub.metaData.identifier,
     name: epub.metaData.title,
-    filePath: path.basename(epubFilepath),
+    filePath: epubFileName,
     pageCount: epub.spine.length,
     faceB64: thumbnail ? thumbnail.toString('base64') : '',
   });
 }
 
-writeFileSync(
-  path.join(bookDir, 'index.json'),
-  JSON.stringify(books, undefined, 2)
-);
+writeFileSync(indexFilePath, JSON.stringify(books, undefined, 2));
